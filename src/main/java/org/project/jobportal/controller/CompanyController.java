6648604a2a3dc291e0398abe20015707579c21bb
Srishty <srishtymangutte@gmail.com>
2025-06-12 20:38:43 +0530

    feat: added /api/comapnies endpoint and controllers

---
+++ src/main/java/org/project/jobportal/controller/CompanyController.java
@@ -0,0 +1,40 @@
+package org.project.jobportal.controller;
+
+import org.project.jobportal.model.Company;
+import org.project.jobportal.service.CompanyService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/companies")
+public class CompanyController {
+
+    @Autowired
+    private CompanyService companyService;
+
+    @GetMapping
+    public List<Company> getAllCompanies() {
+        return companyService.getAllCompanies();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Company> getCompanyById(@PathVariable Long id) {
+        return companyService.getCompanyById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public ResponseEntity<Company> createCompany(@RequestBody Company company) {
+        return ResponseEntity.ok(companyService.createCompany(company));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteCompany(@PathVariable Long id) {
+        companyService.deleteCompany(id);
+        return ResponseEntity.noContent().build();
+    }
+}
