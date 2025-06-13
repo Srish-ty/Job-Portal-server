a6fa1777869120010282ca211aa6997ae40c0642
Srishty <srishtymangutte@gmail.com>
2025-06-13 13:45:33 +0530

    feat: added jwt toekn auth

---
+++ src/main/java/org/project/jobportal/config/JwtAuthFilter.java
@@ -0,0 +1,36 @@
+package org.project.jobportal.config;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import org.project.jobportal.service.JwtService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.lang.NonNull;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+import java.io.IOException;
+
+@Component
+public class JwtAuthFilter extends OncePerRequestFilter {
+
+    @Autowired private JwtService jwtService;
+
+    @Override
+    protected void doFilterInternal(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response,
+                                    @NonNull FilterChain filterChain) throws ServletException, IOException {
+
+        final String authHeader = request.getHeader("Authorization");
+
+        if (authHeader != null && authHeader.startsWith("Bearer ")) {
+            String token = authHeader.substring(7);
+
+            if (!jwtService.isTokenValid(token)) {
+                response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                return;
+            }
+
+            // You could set authentication manually here if needed
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}
